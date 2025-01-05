49df3d179c23eef66f73af0c973d52ed969ae2bb
FacundoAylan <facundogsb@gmail.com>
2025-01-04 21:26:34 -0300

    subiendo arreglos de ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Base URL para los archivos estáticos
   build: {
-    outDir: "dist",
+    outDir: "dist", // Directorio de salida
   },
 })
